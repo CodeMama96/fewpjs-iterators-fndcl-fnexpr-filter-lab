27028b3e43056ee16a9b1f818460bc0f5f4c1d31
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-07 15:42:00 -0500

    commit tree 3eae0cadb3082f5ba1c46ec29e22c89006df43b7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(str){
+//an array of drivers
+const filtered = drivers.filter(driver => driver === 'Bobby')
+
+return findMatching
+}
+
+function fuzzyMatch(){
+
+}
+
+function matchName(){
+
+}
